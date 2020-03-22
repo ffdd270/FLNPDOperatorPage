7b0f851656b419e228ce553ea54a0e7ffc75d07b
ffdd270 <ffdd270@gmail.com>
2020-03-22 23:35:52 +0900

    Refactring Command

--- src/actions/command.ts
+++ src/actions/command.ts
@@ -2,23 +2,26 @@ import {CommandSocket} from "../sockets/command_socket";
 import {DiceResponse, ErrorResponse, Response, TurnResponse, UnknownResponse} from "../util/response";
 import {Dice} from "../instance/dice";
 import {BattleController} from "../controllers/battle";
+import {Battle} from "../instance/battle";
+
+class BattleValueStruct
+{
+    public unit_uid : number;
+    public battle : Battle;
+
+    constructor( unit_uid : number, battle_id : Battle  )
+    {
+        this.unit_uid = unit_uid;
+        this.battle = battle_id;
+    }
+
+}
+
 
 export class CommandActions
 {
-    private static ProcDice(  command : string, params : string  ) : Response
-    {
-        let dice_max = 20;
 
-        if( params != "" )
-        {
-            dice_max = parseInt( params );
-        }
-
-        let dice = new Dice( dice_max );
-        return new DiceResponse(dice);
-    }
-
-    private static ProcTurn(  command : string, params : string, opts : Map<string, string>  ) : Response
+    private static GetBattleValues(  command : string, params : string, opts : Map<string, string> ) : Response | BattleValueStruct
     {
         if( params == "" )
         {
@@ -39,6 +42,36 @@ export class CommandActions
             return new ErrorResponse( command, "WRONG BATTLE ID");
         }
 
+        return new BattleValueStruct( unit_info, battle );
+    }
+
+    private static ProcDice(  command : string, params : string  ) : Response
+    {
+        let dice_max = 20;
+
+        if( params != "" )
+        {
+            dice_max = parseInt( params );
+        }
+
+        let dice = new Dice( dice_max );
+        return new DiceResponse(dice);
+    }
+
+    private static ProcTurn(  command : string, params : string, opts : Map<string, string>  ) : Response
+    {
+        let return_value = this.GetBattleValues( command, params, opts );
+
+        if ( return_value instanceof Response )
+        {
+            return <Response>(return_value);
+        }
+
+        return_value = <BattleValueStruct>( return_value );
+
+        let unit_info = return_value.unit_uid;
+        let battle = return_value.battle;
+
         let unit = battle.SetHaveTurnUnit( unit_info );
         if( unit == null )
         {
@@ -52,10 +85,14 @@ export class CommandActions
 
     private static ProcAttack( command : string, params : string, opts : Map<string, string> ) : Response
     {
-        if( params == "" )
+        let return_value = this.GetBattleValues( command, params, opts );
+
+        if ( return_value instanceof Response )
         {
+            return <Response>(return_value);
         }
 
+        return_value = <BattleValueStruct>( return_value );
     }
 
     static SetCommands()
